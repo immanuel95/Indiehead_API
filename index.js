5c26b129f24d69a15522257dce5829b05e1e31c4
Immanuel <immanuel95.id@gmail.com>
2018-10-17 15:16:30 +0700

    17 Oct 2018

--- index.js
+++ index.js
@@ -17,20 +17,93 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/login", (req, res) => {
-  const sql = `SELECT * FROM userdata`;
+app.get("/", (req, res) => {
+  res.send("<h1>Active</h1>");
+});
 
-  conn.query(sql, (err, result) => {
+////////////////////////GET////////////////////////
+
+app.get("/login", (req, res) => {
+  var { email, username, password } = req.query;
+  var data = {
+    username: username,
+    email: email,
+    password: password
+  };
+  const sql = `SELECT *  FROM userdata WHERE Email LIKE '%${email}%' AND Password LIKE '%${password}%';`;
+
+  conn.query(sql, data, (err, result) => {
     if (err) {
       throw err;
     }
     res.send(result);
+    console.log(result);
   });
 });
 
+app.get("/articles", (req, res) => {
+  const sql = `SELECT * FROM articles`;
+
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+
+    res.send(results);
+  });
+});
+
+app.get("/products", (req, res) => {
+  const sql = `SELECT * FROM productlist p
+                JOIN artist a ON p.Artist = a.idArtist
+                JOIN category c ON  p.Category = c.idCategory`;
+
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+
+    res.send(result);
+  });
+});
+
+app.get("/filtertshirt", (req, res) => {
+  const sql = `SELECT * FROM productlist p
+                JOIN artist a ON p.Artist = a.idArtist
+                JOIN category c ON p.Category = c.idCategory
+                WHERE c.Category LIKE '%CD%'`;
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+
+    res.send(results);
+  });
+});
+
+app.get("/filtercd", (req, res) => {
+  const sql = `SELECT * FROM productlist p
+              JOIN artist a ON p.Artist = a.idArtist
+              JOIN category c ON p.Category = c.idCategory
+              WHERE c.Category LIKE '%CD%'`;
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+
+    res.send(results);
+  });
+});
+
+app.get("/filtervinyl", (req, res) => {
+  const sql = `SELECT * FROM productlist p
+              JOIN artist a ON p.Artist = a.idArtist
+              JOIN category c ON p.Category = c.idCategory
+              WHERE c.Category LIKE '%Vinyl%'`;
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+
+    res.send(results);
+  });
+});
+
+////////////////////////POST////////////////////////
+
 app.post("/register", (req, res) => {
   const userdata = {
-    fullname: req.body.fullname,
+    username: req.body.username,
     email: req.body.email,
     password: req.body.password
   };
