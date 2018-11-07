f70aeca0c3bc4d87f0904cfff464ec546e95e75a
Immanuel <immanuel95.id@gmail.com>
2018-11-07 20:58:39 +0700

    7 Nov 2018 ~adminpage created~

--- index.js
+++ index.js
@@ -119,6 +119,16 @@ app.get("/artists", (req, res) => {
   });
 });
 
+app.get("/category", (req, res) => {
+  const sql = `SELECT * FROM category;`;
+
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+
+    res.send(results);
+  });
+});
+
 app.get("/products", (req, res) => {
   const sql = `SELECT p.*, a.Artist as NamaArtist, c.Category as NamaCategory 
               FROM productlist p
@@ -171,6 +181,47 @@ app.get("/filtervinyl", (req, res) => {
   });
 });
 
+app.put("/products/:id", (req, res) => {
+  const { Picture, Artist, ProductName, Category, Price } = req.body;
+  const data = { Picture, Artist, ProductName, Category, Price };
+
+  const sql = `UPDATE productlist SET ? WHERE idproductlist = ${req.params.id}`;
+  conn.query(sql, data, (err, result) => {
+    if (err) throw err;
+
+    const sql1 = `SELECT p.*, a.Artist as NamaArtist, c.Category as NamaCategory 
+                    FROM productlist p
+                    JOIN artist a ON p.Artist = a.idArtist
+                    JOIN category c ON p.Category = c.idCategory`;
+
+    conn.query(sql1, (err1, result1) => {
+      if (err1) throw err1;
+      res.send(result1);
+    });
+  });
+});
+
+app.post("/addproduct", (req, res) => {
+  const { Picture, ProductName, Artist, Category, Price } = req.body;
+  const data = { Picture, ProductName, Artist, Category, Price };
+
+  const sql = `INSERT INTO productlist SET ?;`;
+
+  conn.query(sql, data, (err, result) => {
+    if (err) throw err;
+
+    const sql1 = `SELECT p.*, a.Artist as NamaArtist, c.Category as NamaCategory 
+                  FROM productlist p
+                  JOIN artist a ON p.Artist = a.idArtist
+                  JOIN category c ON p.Category = c.idCategory`;
+
+    conn.query(sql1, (err1, result1) => {
+      if (err1) throw err1;
+      res.send(result1);
+    });
+  });
+});
+
 // =========================== CART ===========================
 
 app.get("/cart/:username", (req, res) => {
@@ -184,8 +235,6 @@ app.get("/cart/:username", (req, res) => {
     res.send(result);
   });
 });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.post("/cart", (req, res) => {
   const {
@@ -221,7 +270,6 @@ app.post("/cart", (req, res) => {
       conn.query(sql1, (err1, result1) => {
         if (err1) throw err1;
         res.send(result1);
-        console.log("updated");
       });
     } else {
       const sql2 = `INSERT INTO cart SET ?`;
@@ -251,3 +299,90 @@ app.delete("/cart", (req, res) => {
     });
   });
 });
+
+// =========================== TRANSACTIONS ===========================
+
+app.post("/checkout", (req, res) => {
+  const { username, totalPrice } = req.body;
+  const data = { Username: username, TotalPrice: totalPrice };
+
+  const sql = `INSERT INTO transaction SET ?;`;
+
+  conn.query(sql, data, (err, result) => {
+    if (err) throw err;
+
+    const sql1 = `SELECT (SELECT MAX(idtransaction) FROM transaction WHERE Username = '${username}') AS idtransaction,
+                  ProductName, Picture, Price, Amount FROM cart WHERE Username = '${username}'`;
+
+    conn.query(sql1, (err1, result1) => {
+      if (err1) throw err1;
+
+      const sql2 = `INSERT INTO transaction_detail (idtransaction, ProductName, Price, Picture, Amount) VALUES ?`;
+      var values = [];
+      result1.map(data => {
+        values.push([
+          data.idtransaction,
+          data.ProductName,
+          data.Price,
+          data.Picture,
+          data.Amount
+        ]);
+        conn.query(sql2, [values], (err2, result2) => {
+          if (err2) throw err2;
+
+          const sql3 = `DELETE FROM cart WHERE Username = '${username}';`;
+
+          conn.query(sql3, (err3, result3) => {
+            if (err3) throw err3;
+
+            const sql4 = `SELECT c.*, a.Artist as NamaArtist, ct.Category as NamaCategory FROM cart c
+                          JOIN artist a ON c.Artist = a.idArtist
+                          JOIN category ct ON c.Category = ct.idCategory
+                          WHERE c.Username = '${username}';`;
+
+            conn.query(sql4, (err4, result4) => {
+              if (err4) throw err4;
+
+              res.send(result4);
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+app.post("/buynow", (req, res) => {
+  const { username, totalPrice } = req.body;
+  const data = { Username: username, TotalPrice: totalPrice };
+
+  const sql = `INSERT INTO transaction SET ?;`;
+
+  conn.query(sql, data, (err, result) => {
+    if (err) throw err;
+
+    const sql1 = `SELECT (SELECT MAX(idtransaction) FROM transaction WHERE Username = '${username}') AS idtransaction,
+                  ProductName, Picture, Price, Amount FROM cart WHERE Username = '${username}'`;
+
+    conn.query(sql1, (err1, result1) => {
+      if (err1) throw err1;
+
+      const sql2 = `INSERT INTO transaction_detail (idtransaction, ProductName, Price, Picture, Amount) VALUES ?`;
+      var values = [];
+      result1.map(data => {
+        values.push([
+          data.idtransaction,
+          data.ProductName,
+          data.Price,
+          data.Picture,
+          data.Amount
+        ]);
+        conn.query(sql2, [values], (err2, result2) => {
+          if (err2) throw err2;
+        });
+      });
+    });
+  });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
